a64e0e87935704407b6eee433cbffef25e8a0029
jean de dieu <jeandedieusessou@gmail.com>
2024-10-14 17:56:19 +0000

    feat: install react router dom and setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import Header from "./components/Layouts/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
-    <>
-      <Header />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/github" element={"https://github.com/johnOfGod33"} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Layouts/Header";
+import Intro from "../components/Layouts/Intro";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Intro />
+    </>
+  );
+};
+
+export default Home;
